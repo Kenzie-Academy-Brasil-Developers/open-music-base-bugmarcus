efeca877dc2fd882eab6b10eacd3813d4abbf2f4
root <root@Skeets>
2023-08-17 12:16:22 -0300

    Entrega pronta conforme criterios, porem ainda falta aumentar a responsividade

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,12 +1,28 @@
-const darkModeButton = document.querySelector(".dark-mode-selector");
-const body = document.body;
-const darkModeEnabled = localStorage.getItem("darkModeEnabled") === "true";
+window.addEventListener("load", () => {
+  const isDarkMode = localStorage.getItem("darkModeEnabled") === "true";
+  const body = document.body;
+  const darkModeButton = document.querySelector(".dark-mode-selector");
+  const icon = darkModeButton.querySelector("i");
 
-if (darkModeEnabled) {
-  body.classList.add("dark-mode");
-}
+  // Função para atualizar o ícone do botão
+  function updateDarkModeButtonIcon(isDarkMode) {
+    icon.classList.toggle("fa-moon", !isDarkMode);
+    icon.classList.toggle("fa-sun", isDarkMode);
+  }
 
-darkModeButton.addEventListener("click", function () {
-  body.classList.toggle("dark-mode");
-  localStorage.setItem("darkModeEnabled", body.classList.contains("dark-mode"));
+  // Inicializa o estado do dark mode e ícone do botão
+  if (isDarkMode) {
+    body.classList.add("dark-mode");
+    updateDarkModeButtonIcon(true);
+  } else {
+    icon.classList.add("fa-moon");
+  }
+
+  // Evento de clique do botão para alternar o dark mode
+  darkModeButton.addEventListener("click", function () {
+    body.classList.toggle("dark-mode");
+    const isDarkMode = body.classList.contains("dark-mode");
+    localStorage.setItem("darkModeEnabled", isDarkMode);
+    updateDarkModeButtonIcon(isDarkMode);
+  });
 });
